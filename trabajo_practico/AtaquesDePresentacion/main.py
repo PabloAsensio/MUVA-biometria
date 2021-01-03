3c4bfdcf877dcb2fb1a6c854cd991f8206d5919a
Pablo Asensio Martínez <p.asensio97@gmail.com>
2021-01-03 02:42:40 +0100

    Update main.py

--- trabajo_practico/AtaquesDePresentacion/main.py
+++ trabajo_practico/AtaquesDePresentacion/main.py
@@ -1,67 +1,26 @@
-import numpy as np
 import cv2 as cv
-import os
-import sys
-from classifiers import calculate_histogram, calculate_label
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
-# img = cv2.imread("./FravAttack/FLIR/Rainbow/front/users/USER_024.JPG")
-
-
-# cv2.imshow("Main Face", img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-
-def get_absolute_path(relative_path):
-    return os.path.realpath(relative_path)
-
-
-def check_path(path):
-    return os.path.exists(path)
-
-
-def get_all_images_paths(root, ext=".jpg"):
-    if not check_path(root):
-        print("Path doesn't exist.")
-        sys.exit(1)
-
-    print("Getting all images paths...")
-    images_list = []
-    for path, _, files in os.walk(root):
-        for name in files:
-            if name.lower().endswith(ext):
-                images_list.append(get_absolute_path(os.path.join(path, name)))
-    print("Done! (Getting all images paths).")
-    return images_list
-
+from classifiers import do_classification, histogram_features
+from common import get_all_images_paths
+from metrics import metric
 
 if __name__ == "__main__":
-    raw_img_list = get_all_images_paths(
+    img_list = get_all_images_paths(
         "./FravAttack/FLIR/Rainbow/front", ext=".jpg"
     )  # Change to your dataset path
 
-    # create pandas dataframe
-    features_df = pd.DataFrame()
-
-    # select output path
-    output_path = "histogram_features.csv"
-
-    for _, individual in enumerate(raw_img_list):
-        # calulate features and label of picture
-        hist = calculate_histogram(individual)
-        label = calculate_label(individual)
-
-        # append labels and path to features
-        hist = np.append(hist, label)
-        hist = np.append(hist, individual)
-
-        # data to df
-        features_df = features_df.append(
-            pd.DataFrame(data=hist).transpose(), ignore_index=True
-        )
+    # features_df = histogram_features(img_list)
 
     # # DECOMMENT if you wanna store df to .csv
-    # features_df.to_csv(output_path, index=False)
-    print(features_df[769][149])
+    # features_df.to_csv("histogram_features.csv", index=False)
+
+    features_df = pd.read_csv("histogram_features.csv", header=0, delimiter=",")
+
+    # Train and test soft voting classifier
+    (y_prob, y_predic, y_real) = do_classification(features_df)
+
+    # Generate APCER-BPCER-curve
+    metric(y_prob, y_predic, y_real, 1)
