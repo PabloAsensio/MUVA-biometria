ee90c76eac030382c25ee786a148a1867616de41
pasensio97 <p.asensio97@gmail.com>
2021-01-10 02:29:00 +0100

    Update main.py

--- trabajo_practico/DeteccionDeMinucias/main.py
+++ trabajo_practico/DeteccionDeMinucias/main.py
@@ -5,7 +5,54 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from skimage.filters import gabor
 from skimage.morphology import skeletonize
+
+# Eliminar parte de la imagen que se queda fuera de la huella
+def getFingerprintRegion(img):
+    row1 = 0
+    row2 = 0
+    col1 = 0
+    col2 = 0
+    for row in range(img.shape[0]):
+        row1 = row
+        if np.sum(img[row, 0 : img.shape[1]]) > 10:
+            break
+
+    for row in range(img.shape[0]):
+        row2 = img.shape[0] - row - 1
+        if np.sum(img[row2, 0 : img.shape[1]]) > 10:
+            break
+
+    for row in range(img.shape[1]):
+        col1 = row
+        if np.sum(img[0 : img.shape[0], col1]) > 10:
+            break
+
+    for row in range(img.shape[1]):
+        col2 = img.shape[0] - row - 1
+        if np.sum(img[0 : img.shape[0], col2]) > 10:
+            break
+
+    return row1, row2, col1, col2
+
+
+def cutImage(img):
+    kernel = np.ones((5, 5), dtype=np.uint8)
+
+    _, erosion = gabor(img, 0.7, theta=0)
+    erosion = cv2.morphologyEx(erosion, cv2.MORPH_OPEN, kernel)
+
+    row1, row2, col1, col2 = getFingerprintRegion(erosion)
+
+    h, w = img.shape
+    for i in range(h):
+        for j in range(w):
+            if not ((i < row2) and (i > row1) and (j > col1) and (j < col2)):
+                if img[i, j] != 255:
+                    img[i, j] = 255
+
+    return img.copy(), row1, row2, col1, col2
 
 
 def isBifurcation(row, col, img):
@@ -40,50 +87,99 @@ def isEnding(row, col, img):
     return False
 
 
-if __name__ == "__main__":
-    img = "./huellasFVC2004/101_3.tif"
+def deginrad(degree):
+    radiant = 2 * np.pi / 360 * degree
+    return radiant
 
+
+def gaborFilter(img):
+    _, filt_imag = gabor(
+        img,
+        frequency=0.1,
+        theta=deginrad(90),
+        bandwidth=3,
+        sigma_x=None,
+        sigma_y=None,
+        n_stds=3,
+        offset=0,
+        mode="reflect",
+        cval=0,
+    )
+    return filt_imag.copy()
+
+
+# Comprueba que no hay una minucia del mismo tipo en una vecindad 3x3
+def isMinutaeClose(i, j, typeMinutae, l):
+    if (
+        [i - 1, j - 1, typeMinutae] in l
+        or [i - 1, j, typeMinutae] in l
+        or [i - 1, j + 1, typeMinutae] in l
+        or [i + 1, j - 1, typeMinutae] in l
+        or [i + 1, j, typeMinutae] in l
+        or [i + 1, j + 1, typeMinutae] in l
+        or [i, j - 1, typeMinutae] in l
+        or [i, j + 1, typeMinutae] in l
+    ):
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    plot = True
+    img = "./huellasFVC2004/103_8.tif"
+
+    # se lee la imagen de la huella
     image = cv2.imread(img, 0)
 
-    plt.matshow(image, cmap="gray")
-    plt.title("original")
-    fig1 = plt.figure(1)
+    if plot:
+        cv2.imshow("original", image)
 
+    # se corta la imagen para que solo quede la huella, davuelve los margenes tambien
+    image, row1, row2, col1, col2 = cutImage(image)
+    if plot:
+        cv2.imshow("cutted", image)
+
+    # se aplica un filtlo de gabor que devuelve la imagen binarizada
+    image = gaborFilter(image)
+    img_gabor = image.copy()  # se guarda para mostras las minucias despues
+    if plot:
+        cv2.imshow("gavor", image)
+
+    # se umbraliza la imagen adaptavivamente
     image = cv2.adaptiveThreshold(
         np.invert(image), 1, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2
     )
+    if plot:
+        cv2.imshow("gabor; adaptiveThreshold", image * 255)
 
-    plt.matshow(image, cmap="gray")
-    plt.title("invert image; adaptiveThreshold")
-    fig2 = plt.figure(2)
-
-    # contours, hierarchy = cv2.findContours(image, 1, 2)
-
-    # for contour in contours:
-    #     print(contour)
-
-    # plt.matshow(image, cmap="gray")
-    # plt.title("invert image; adaptiveThreshold; Contornos")
-    # fig3 = plt.figure(3)
-
+    # se realiza un esqueleto de la huella
     sklen = skeletonize(image)
+    if plot:
+        plt.imshow(sklen, cmap="gray")
+        plt.title("gabor; adaptiveThreshold; skeleton")
 
-    plt.matshow(sklen, cmap="gray")
-    plt.title("skleton")
-    fig4 = plt.figure(4)
-
+    # se cuentan las minucias dadas unas condiciones
     rows, cols = sklen.shape
-
     minutaes = []
+    for i in range(rows):
+        for j in range(cols):
+            # se mira en aquellos puntos donde haya esqueleto
+            if sklen[i, j]:
+                # solo se miran los pixeles donde hay huella y formen parte del filtro de gabor (valen distinto de 0 ~False)
+                if (
+                    (i < row2)
+                    and (i > row1)
+                    and (j > col1)
+                    and (j < col2)
+                    and img_gabor[i][j]
+                ):
+                    if isBifurcation(i, j, sklen) and not isMinutaeClose(
+                        i, j, 3, minutaes
+                    ):
+                        minutaes.append([i, j, 3])
 
-    for i in range(1, rows - 1):
-        for j in range(1, cols - 1):
-            if sklen[i][j]:
-                if isBifurcation(i, j, sklen):
-                    minutaes.append([i, j, 3])
-
-                if isEnding(i, j, sklen):
-                    minutaes.append([i, j, 1])
+                    if isEnding(i, j, sklen) and not isMinutaeClose(i, j, 1, minutaes):
+                        minutaes.append([i, j, 1])
 
     minucias = pd.DataFrame.from_records(minutaes)
     minucias.columns = ["row", "col", "label"]
@@ -91,19 +187,26 @@ if __name__ == "__main__":
     terminaciones = minucias[minucias["label"] == 1].drop(columns="label")
     bifurcaciones = minucias[minucias["label"] == 3].drop(columns="label")
 
-    img = plt.imread(img)
+    if plot:
+        img = plt.imread(img)
 
-    fig5 = plt.figure(5)
+        fig5 = plt.figure(5)
 
-    plt.plot(terminaciones["col"], terminaciones["row"], "b.", label="terminaciones")
-    plt.plot(bifurcaciones["col"], bifurcaciones["row"], "r.", label="bifurcaciones")
+        plt.plot(
+            terminaciones["col"], terminaciones["row"], "b.", label="terminaciones"
+        )
+        plt.plot(
+            bifurcaciones["col"], bifurcaciones["row"], "r.", label="bifurcaciones"
+        )
 
-    plt.imshow(img, cmap="gray")
-    plt.legend(loc="best")
-    plt.title("minutaes")
+        plt.imshow(img, cmap="gray")
+        plt.legend(loc="best")
+        plt.title("minutaes")
 
-    plt.show()
+        plt.show()
 
-    # df.to_csv("minutaes.csv", sep=";")
+    # se guarda un txt con las minucias
+    minucias.to_csv("minutaes.txt", sep=";")
 
-    # plt.show()
+    cv2.waitKey()
+    cv2.destroyAllWindows()
